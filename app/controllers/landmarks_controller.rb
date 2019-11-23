49e03e491f631b35e014d1582ef858e94bb47f07
Karen V <karenvo@usc.edu>
2019-11-23 11:13:56 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,20 @@
 class LandmarksController < ApplicationController
   # add controller methods
+  get '/landmarks/new' do
+
+  end
+
+  post '/landmarks' do
+
+  end
+
+  get '/landmarks/all' do
+    @landmarks = Landmark.all
+    erb :'/landmarks/all'
+  end
+
+  get '/landmarks/:id' do
+    # find by id
+  end
+
 end
