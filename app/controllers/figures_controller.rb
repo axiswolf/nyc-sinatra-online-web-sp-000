91b76c4ff9b7a952e80c49aaa81f15c072afcb3d
Karen V <karenvo@usc.edu>
2019-11-23 10:44:59 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -10,29 +10,31 @@ class FiguresController < ApplicationController
   end
 
   post '/figures' do
-    # check each name field to see if it is not empty
-    # if it is empty, shovel name into array
-    # title_params = params["title"]["name"]
-    # landmark_params = params["landmark"]
-    # @figure = Figure.create(params["figure"])
-    @figure = Figure.find_or_create_by(name: params["figure_name"])
-    if !params["title"]["name"].empty?
-      @figure.titles << Title.create(name:params["title"]["name"])
+
+      @figure = Figure.create(params["figure"])
+      #figure = Figure.find_or_create_by(name: params["figure_name"])
+
+      title_params = params["title"]["name"]
+      landmark_params = params["landmark"]
+
+      if !title_params.empty?
+        title = Title.create(params["title"])
+        @figure.titles << title
+      end
+
+      if !landmark_params.empty?
+          landmark = Landmark.create(landmark_params)
+          @figure.landmarks << landmark
+      end
+      @figure.save
+
+      redirect to "/figures/#{@figure.id}" #moves to route GET '/figures/:id'
     end
-
-    if !params["landmark"].empty?
-      landmark = Landmark.create(landmark_params)
-      @figure.landmarks << landmark
-    end
-    @figure.save
-    puts params
-
-    redirect to "figures/#{@figure.id}"
-
-  end
 
     get '/figures/:id' do
+
       @figure = Figure.find_by(id: params["id"])
+
       erb :'figures/show'
     end
 
